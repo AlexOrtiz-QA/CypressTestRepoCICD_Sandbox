04d80ccae56f5163503f65166b89024268cb4776
AlexOrtiz <alejandro.ortiz.bba@outlook.com>
2026-07-05 00:00:16 -0600

    Added new testData and it block in TC02

--- cypress/e2e/Tc02_Login.cy.js
+++ cypress/e2e/Tc02_Login.cy.js
@@ -18,4 +18,12 @@ describe('Login Functionality', function(){
         cy.get('[name="password"]').type(testData.pwd);
         cy.get('.btn').click();
     })
+
+    it('Unsuccessfull Login', function(){
+        cy.visit('https://demo.realworld.show/');
+        cy.get('.container > .nav > :nth-child(2) > .nav-link').click();
+        
+        cy.get('[name="email"]').type(testData.wrongEmail);
+        cy.get('[name="password"]').type(testData.Wrongpwd);
+        cy.get('.btn').click();
 })
